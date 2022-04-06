e5d7c36fd4138f43697cb990f8e32fae87cbd816
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-06 12:35:15 +0300

    Delete Execute_Keylogger.py

--- Execute_Keylogger.py
+++
@@ -1,8 +0,0 @@
-import os
-class Execute_Keylogger():
-	def __init__(self):
-		self.Main()
-
-	def Main(self):
-		dirname = "\\".join(os.path.abspath(__file__).split("\\")[:-1])
-		os.system('"' + dirname + '\\python.exe" "' + dirname + '\\Keylogger.py"')
