1898cf1628dea7745e9fdbff8eef1c9aaa1527ae
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-06 13:28:28 +0300

    Update Ask_Admin.py

--- Ask_Admin.py
+++ Ask_Admin.py
@@ -1,18 +1,18 @@
 import sys
 import os
-import win32com.shell.shell as shell
 class Ask_Admin():
 	def __init__(self):
 		self.Main()
 
 	def Main(self):
+		import win32com.shell.shell as shell
 		if sys.argv[-1] != 'asadmin':
 			script = os.path.abspath(sys.argv[0])
 			params = ' '.join([script] + sys.argv[1:] + ['asadmin'])
 			while True:
-				#try:
+				try:
 					print(sys.executable + " " + params)
 					shell.ShellExecuteEx(lpVerb='runas', lpFile="cmd.exe", lpParameters="/c " + sys.executable + " " + params)
 					os.system("taskkill /IM python.exe /F")
-				#except Exception as ex:
-				#	pass
+				except Exception as ex:
+					pass
