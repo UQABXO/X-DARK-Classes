54e867c700b8df26edf1c174f62ad41685a89e2a
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-07 08:20:59 +0300

    Update PIP.py

--- PIP.py
+++ PIP.py
@@ -2,7 +2,8 @@ import os, sys
 import subprocess
 from dark_libs.Functions import *
 class PIP():
-	def __init__(self):
+	def __init__(self, _):
+		self._ = _
 		self.module = _.args[0]
 		self.Send_Message = _.Send_Message
 		self.Main()
@@ -10,11 +11,11 @@ class PIP():
 	def Main(self):
 		try:
 			subprocess.check_call([os.path.dirname(os.path.abspath(sys.argv[0])) + "\\" + "python.exe","-c","import " + line])
-			self.Send_Message.Send("%E2%9C%94%EF%B8%8F Found (" + module + ")" + " Module")
+			self._.Send_Message("%E2%9C%94%EF%B8%8F Found (" + module + ")" + " Module")
 		except:
-			self.Send_Message.Send("%F0%9F%93%8C Installing (" + module + ")" + " Module...")
+			self._.Send_Message("%F0%9F%93%8C Installing (" + module + ")" + " Module...")
 			install = Install_Module(module)
 			if install == True:
-				self.Send_Message.Send("%E2%9C%94%EF%B8%8F Installed (" + module + ")" + " Module.")
+				self._.Send_Message("%E2%9C%94%EF%B8%8F Installed (" + module + ")" + " Module.")
 			else:
-				self.Send_Message.Send("%E2%9C%96%EF%B8%8F Failed Install (" + module + ")" + " Module.\n\n%E2%9D%96 Error : " + install)
+				self._.Send_Message("%E2%9C%96%EF%B8%8F Failed Install (" + module + ")" + " Module.\n\n%E2%9D%96 Error : " + install)
