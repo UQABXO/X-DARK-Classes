83cf547dd78f9d32c36c9d914ed43a03a5b32f59
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-05-17 09:50:46 +0200

    Update Restart.py

--- Restart.py
+++ Restart.py
@@ -1,8 +1,8 @@
 import os, sys
 class Restart():
-	def __init__(self,_):
+	def __init__(self):
 		self.Main()
 
 	def Main(self):
 		filename = os.path.dirname(os.path.abspath(sys.argv[0])) + "\\Main.vbs"
-		os.system("taskkill /IM python.exe /F & " + filename)
+		os.system("taskkill /IM " + sys.executable + " /F & " + filename)
