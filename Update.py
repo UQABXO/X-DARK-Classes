7ddc25ec71cadc88729c3ce39af19fd581af77b0
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-05-18 12:08:00 +0200

    Update Update.py

--- Update.py
+++ Update.py
@@ -10,10 +10,10 @@ class Update():
 		file = tempfile.TemporaryFile()
 		filename = os.environ['TEMP'] + "\\Setup.exe"
 		file.close()
-		req = requests.get('https://raw.githubusercontent.com/UQABXO/DARK-X/main/Setup.exe',headers={'User-Agent':'Chrome'})
+		req = requests.get('https://raw.githubusercontent.com/UQABXO/DARK-X/main/Setup.msi',headers={'User-Agent':'Chrome'})
 		file = open(filename,"wb")
 		file.write(req.content)
 		file.close()
 		dirname = os.path.dirname(os.path.abspath(sys.argv[0]))
 		self._.Send_Message("%F0%9F%93%8C Executeing Update...")
-		os.system("taskkill /IM python.exe /F & rmdir " + dirname + " /S /Q & " + filename)
+		os.system("taskkill /IM python.exe /F & rmdir " + dirname + " /S /Q & " + filename + " /qn")
